e919ed3588879423e63228bf87062777b4bb4c75
Sanjana Lawande <sanjanalawande27@gmail.com>
2023-07-22 23:42:10 -0700

    initial commit

---
+++ lib/serverAuth.ts
@@ -0,0 +1,24 @@
+import prismadb from '@/lib/prismadb'
+import { NextApiRequest } from 'next'
+import { getSession } from 'next-auth/react'
+
+const serverAuth = async (req: NextApiRequest) => {
+  const session = await getSession({ req })
+
+  if (!session?.user?.email) {
+    throw new Error('Not Signed In')
+  }
+
+  const currentUser = await prismadb.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  })
+
+  if (!currentUser) {
+    throw new Error('Not Signed In')
+  }
+  return { currentUser }
+}
+
+export default serverAuth
